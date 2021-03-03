6dd630c60cc73b5c6553dcd713cdc941eb7debb6
Keven Max <kmax136@gmail.com>
2021-03-02 22:03:00 -0300

    feat: Add pokemon list

---
+++ client/pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import { useFetch } from './hook/useFetch';
+
+export default function Home(props) {
+
+  const { data: pokemons } = useFetch("https://pokeapi.co/api/v2/pokedex/2");
+
+  console.log(pokemons)
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <ul>
+          {pokemons && pokemons.pokemon_entries.map((pokemon) => {
+            return (
+              <li key={pokemon.entry_number}>
+                <Link href={`pokemons/${pokemon.entry_number}`}>
+                  <a>{pokemon.pokemon_species.name}</a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+};
